901282a8512ed9f6ef04cb74c1ea609dee6a4279
girvel <widauka@ya.ru>
2025-08-14 17:47:22 +0500

    Dummy level

---
+++ conf.lua
@@ -0,0 +1,8 @@
+love.conf = function(t)
+  t.console = true
+  -- t.window.title = "Fallen"
+  -- t.window.icon = "assets/sprites/moose_dude.png"
+  -- t.window.resizable = true
+  -- t.window.fullscreen = true
+end
+
---
+++ levels/main/init.lua
@@ -0,0 +1,8 @@
+return {
+  --- @type level_config
+  config = {
+    grid_layers = {"solids"},
+    grid_complex_layers = {},
+    cell_size = 16,
+  },
+}
