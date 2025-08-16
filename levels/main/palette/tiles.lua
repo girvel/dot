e97214b60c17b36e6105cc12d0b53c70693c63b8
girvel <widauka@ya.ru>
2025-08-16 14:54:27 +0500

    Use factoring

--- levels/main/palette/tiles.lua
+++ levels/main/palette/tiles.lua
@@ -1,15 +1,6 @@
-local tiles = {}
+local factoring = require("engine.tech.factoring")
 
-tiles.ATLAS_IMAGE = love.graphics.newImage("assets/sprites/atlases/tiles.png")
-
-tiles[1] = function()
-  return {
-    sprite = {
-      type = "atlas",
-      -- TODO! cell_size should be in level definition
-      quad = love.graphics.newQuad(0, 0, 16, 16, tiles.ATLAS_IMAGE:getDimensions()),
-    }
-  }
-end
-
-return tiles
+-- TODO! cell_size should be in level definition
+return factoring.from_atlas("assets/sprites/atlases/tiles.png", 16, {
+  "grass",
+})
