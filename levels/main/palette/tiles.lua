b9f149d379bc60cc93b6172211f58989067f21b8
girvel <widauka@ya.ru>
2025-08-16 14:58:54 +0500

    Two different tiles

--- levels/main/palette/tiles.lua
+++ levels/main/palette/tiles.lua
@@ -2,5 +2,5 @@ local factoring = require("engine.tech.factoring")
 
 -- TODO! cell_size should be in level definition
 return factoring.from_atlas("assets/sprites/atlases/tiles.png", 16, {
-  "grass",
+  "light_grass", "dark_grass",
 })
