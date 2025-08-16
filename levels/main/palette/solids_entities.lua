576c88715818808410c4a483888eba15db634979
girvel <widauka@ya.ru>
2025-08-17 03:04:09 +0500

    Acting tester entity

--- levels/main/palette/solids_entities.lua
+++ levels/main/palette/solids_entities.lua
@@ -1,12 +1,35 @@
+local level = require("engine.tech.level")
+
+
 local solids_entities = {}
 
 solids_entities.player = function()
   return {
     codename = "player",
     player_flag = true,
+    transparent_flag = true,
+    fov_r = 15,
     sprite = {
       type = "image",
       image = love.graphics.newImage("engine/assets/sprites/moose_dude.png"),
+    },
+  }
+end
+
+solids_entities.ai_tester = function()
+  return {
+    codename = "ai_tester",
+    transparent_flag = true,
+    sprite = {
+      type = "image",
+      image = love.graphics.newImage("engine/assets/sprites/moose_dude.png"),
+    },
+    ai = {
+      run = function(entity, dt)
+        if Random.chance(1 / 60) then
+          level.safe_move(entity, entity.position + Random.choice(Vector.directions))
+        end
+      end,
     },
   }
 end
