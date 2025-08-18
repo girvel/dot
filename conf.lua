35afebc34f988e1e6408e98ff36d334f110c4a7f
girvel <widauka@ya.ru>
2025-08-18 05:07:43 +0500

    Sketch the level size

--- conf.lua
+++ conf.lua
@@ -4,5 +4,7 @@ love.conf = function(t)
   -- t.window.icon = "assets/sprites/moose_dude.png"
   -- t.window.resizable = true
   -- t.window.fullscreen = true
+  t.window.width = 1920
+  t.window.height = 1080
 end
 
