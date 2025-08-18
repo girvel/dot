f11235d2c6b6479a1ca245af24523241007c051a
girvel <widauka@ya.ru>
2025-08-18 14:40:18 +0500

    Tree trunks, continue sketching the level

--- levels/main/palette/solids.lua
+++ levels/main/palette/solids.lua
@@ -1,6 +1,10 @@
 local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
 
+local lows = Table.set {
+  "slope_2", "slope_3", "slope_5", "slope_6",
+}
+
 return factoring.from_atlas(
   "assets/sprites/atlases/solids.png", config.cell_size,
   {
@@ -8,13 +12,14 @@ return factoring.from_atlas(
     "wall_5",  "wall_6",  "wall_7",  "wall_8",  "hutwall_5",  "hutwall_6",  "hutwall_7",  "hutwall_8",
     "wall_9",  "wall_10", "wall_11", "wall_12", "hutwall_9",  "hutwall_10", "hutwall_11", "hutwall_12",
     "wall_13", "wall_14", "wall_15", "wall_16", "hutwall_13", "hutwall_14", "hutwall_15", "hutwall_16",
-    false, "slope_1", false, false, false, false, false, false,
-    "slope_2", "slope_3", "slope_4", false, false, false, false, false,
-    false, "slope_5", false, false, false, false, false, false,
+    "slope_1", "slope_2", false,     "trunk_1", "trunk_2",    false,        false,        false,
+    "slope_3", "slope_4", "slope_5", "trunk_3", "trunk_4",    false,        false,        false,
+    false,     "slope_6", false,     "trunk_5", "trunk_6",    false,        false,        false,
   },
   function(codename)
     return {
       transparent_flag = codename:starts_with("slope") or nil,
+      low_flag = lows[codename],
     }
   end
 )
