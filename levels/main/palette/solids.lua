0b254c362490cc6fc149abae479b032b2030a147
girvel <widauka@ya.ru>
2025-08-18 05:07:30 +0500

    New tiles, hut walls

--- levels/main/palette/solids.lua
+++ levels/main/palette/solids.lua
@@ -2,8 +2,8 @@ local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
 
 return factoring.from_atlas("assets/sprites/atlases/solids.png", config.cell_size, {
-  "wall_1",  "wall_2",  "wall_3",  "wall_4",  false, false, false, false,
-  "wall_5",  "wall_6",  "wall_7",  "wall_8",  false, false, false, false,
-  "wall_9",  "wall_10", "wall_11", "wall_12", false, false, false, false,
-  "wall_13", "wall_14", "wall_15", "wall_16", false, false, false, false,
+  "wall_1",  "wall_2",  "wall_3",  "wall_4",  "hutwall_1",  "hutwall_2",  "hutwall_3",  "hutwall_4", 
+  "wall_5",  "wall_6",  "wall_7",  "wall_8",  "hutwall_5",  "hutwall_6",  "hutwall_7",  "hutwall_8", 
+  "wall_9",  "wall_10", "wall_11", "wall_12", "hutwall_9",  "hutwall_10", "hutwall_11", "hutwall_12",
+  "wall_13", "wall_14", "wall_15", "wall_16", "hutwall_13", "hutwall_14", "hutwall_15", "hutwall_16",
 })
--- levels/main/palette/tiles.lua
+++ levels/main/palette/tiles.lua
@@ -2,7 +2,7 @@ local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
 
 return factoring.from_atlas("assets/sprites/atlases/tiles.png", config.cell_size, {
-  "grass_1", "grass_2", "dirt_1", "dirt_2", "leaves", "roots", "flowers_1", false,
+  "grass_1", "grass_2", "dirt_1", "dirt_2", "leaves", "roots", "flowers_1", "planks",
   false, false, false, false, false, false, "flowers_2", false,
   false, false, false, false, false, false, "flowers_3", false,
   false, false, false, false, false, false, "flowers_4", false,
