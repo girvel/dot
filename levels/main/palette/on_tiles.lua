eac23638df6793a36ed2ae40f71a6345eee28768
girvel <widauka@ya.ru>
2025-08-18 16:26:38 +0500

    Snow top tile

---
+++ levels/main/palette/on_tiles.lua
@@ -0,0 +1,9 @@
+local factoring = require("engine.tech.factoring")
+local config = require("levels.main.config")
+
+return factoring.from_atlas("assets/sprites/atlases/on_tiles.png", config.cell_size, {
+  "snow_1",  "snow_2",  "snow_3",  "snow_4", false, false, false, false,
+  "snow_5",  "snow_6",  "snow_7",  "snow_8", false, false, false, false,
+  "snow_9",  "snow_10", "snow_11", "snow_12", false, false, false, false,
+  "snow_13", "snow_14", "snow_15", "snow_16", false, false, false, false,
+})
