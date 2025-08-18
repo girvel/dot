bbc7bf06eb56811f08a54f4f94c3416207d18c83
girvel <widauka@ya.ru>
2025-08-18 18:11:08 +0500

    Reorganize, snow tile for bordering water

--- levels/main/palette/tiles.lua
+++ levels/main/palette/tiles.lua
@@ -3,7 +3,7 @@ local config = require("levels.main.config")
 
 return factoring.from_atlas("assets/sprites/atlases/tiles.png", config.cell_size, {
   "grass_1", "grass_2", "dirt_1", "dirt_2", "leaves", "roots", "flowers_1", "planks",
-  false, false, "gravel_1", "gravel_2", false, false, "flowers_2", false,
+  false, false, "gravel_1", "gravel_2", "snow", false, "flowers_2", false,
   false, false, false, false, false, false, "flowers_3", false,
   false, false, false, false, false, false, "flowers_4", false,
 })
--- levels/main/palette/tiles_entities.lua
+++ levels/main/palette/tiles_entities.lua
@@ -1,6 +1,6 @@
 local _common = require "levels.main.palette._common"
 local tiles_entities = {}
 
-tiles_entities.water = _common.water(Vector.down * .5)
+tiles_entities.walkable_water = _common.water(Vector.down * .5)
 
 return tiles_entities
