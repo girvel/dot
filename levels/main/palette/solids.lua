b7136198509d3a68bad5ce600f01fd72c1e0bafd
girvel <widauka@ya.ru>
2025-08-18 14:02:18 +0500

    Slope sprite

--- levels/main/palette/solids.lua
+++ levels/main/palette/solids.lua
@@ -1,9 +1,20 @@
 local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
 
-return factoring.from_atlas("assets/sprites/atlases/solids.png", config.cell_size, {
-  "wall_1",  "wall_2",  "wall_3",  "wall_4",  "hutwall_1",  "hutwall_2",  "hutwall_3",  "hutwall_4", 
-  "wall_5",  "wall_6",  "wall_7",  "wall_8",  "hutwall_5",  "hutwall_6",  "hutwall_7",  "hutwall_8", 
-  "wall_9",  "wall_10", "wall_11", "wall_12", "hutwall_9",  "hutwall_10", "hutwall_11", "hutwall_12",
-  "wall_13", "wall_14", "wall_15", "wall_16", "hutwall_13", "hutwall_14", "hutwall_15", "hutwall_16",
-})
+return factoring.from_atlas(
+  "assets/sprites/atlases/solids.png", config.cell_size,
+  {
+    "wall_1",  "wall_2",  "wall_3",  "wall_4",  "hutwall_1",  "hutwall_2",  "hutwall_3",  "hutwall_4",
+    "wall_5",  "wall_6",  "wall_7",  "wall_8",  "hutwall_5",  "hutwall_6",  "hutwall_7",  "hutwall_8",
+    "wall_9",  "wall_10", "wall_11", "wall_12", "hutwall_9",  "hutwall_10", "hutwall_11", "hutwall_12",
+    "wall_13", "wall_14", "wall_15", "wall_16", "hutwall_13", "hutwall_14", "hutwall_15", "hutwall_16",
+    false, "slope_1", false, false, false, false, false, false,
+    "slope_2", "slope_3", "slope_4", false, false, false, false, false,
+    false, "slope_5", false, false, false, false, false, false,
+  },
+  function(codename)
+    return {
+      transparent_flag = codename:starts_with("slope") or nil,
+    }
+  end
+)
