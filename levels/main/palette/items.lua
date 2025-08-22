3057334e218b336c7db7770f8ddd3dd1ba412700
girvel <widauka@ya.ru>
2025-08-22 23:01:15 +0500

    Futile attempts to write the screenplay

--- levels/main/palette/items.lua
+++ levels/main/palette/items.lua
@@ -4,7 +4,7 @@ local items = {}
 items.knife = function()
   return Table.extend(animated.mixin("engine/assets/sprites/animations/knife"), {
     name = "кухонный нож",
-    direction = Vector.right,
+    codename = "knife",
     -- damage_roll = D(2),
     -- bonus = 1,
     -- tags = {
