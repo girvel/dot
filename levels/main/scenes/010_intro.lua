a6d95244daac4e2256864083d733490742de36a5
girvel <widauka@ya.ru>
2025-08-29 23:24:40 +0500

    Implement more of 010_intro

--- levels/main/scenes/010_intro.lua
+++ levels/main/scenes/010_intro.lua
@@ -7,6 +7,7 @@ return {
     characters = {
       player = {},
       khaned = {},
+      likka = {},
     },
 
     start_predicate = function(self, dt, ch)
@@ -15,6 +16,9 @@ return {
 
     run = function(self, ch)
       local player = screenplayer.new("assets/screenplay/010_intro.ms", ch)
+        ch.khaned:rotate(Vector.up)
+        ch.likka:rotate(Vector.up)
+
         player:lines()
         local options = player:start_options()
         for _ = 1, 3 do
@@ -23,6 +27,19 @@ return {
             player:lines()
           player:finish_option()
         end
+        player:finish_options()
+        player:lines()
+
+        api.rotate(ch.likka, ch.khaned)
+
+        player:lines()
+
+        local n = api.options(player:start_options())
+          if n == 1 then
+            player:start_option(n)
+              -- NEXT! branch
+            player:finish_option()
+          end
         player:finish_options()
       player:finish()
     end,
