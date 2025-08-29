66fa05e66f386ae9eabf9358ed38d39c2223b22d
girvel <widauka@ya.ru>
2025-08-30 00:48:44 +0500

    Bushes

--- levels/main/palette/on_solids.lua
+++ levels/main/palette/on_solids.lua
@@ -2,7 +2,7 @@ local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
 
 return factoring.from_atlas("assets/sprites/atlases/on_solids.png", config.cell_size, {
-  false, false, false, false, false, false, false, false,
+  "vines", "grass_1", "grass_2", false, false, false, false, false,
   false, false, false, false, false, false, false, false,
   false, false, false, false, false, false, false, false,
   false, false, false, false, false, false, false, false,
--- levels/main/palette/solids.lua
+++ levels/main/palette/solids.lua
@@ -5,6 +5,11 @@ local lows = Table.set {
   "slope_2", "slope_3", "slope_5", "slope_6",
 }
 
+local transparents = Table.set {
+  "slope_1", "slope_2", "slope_3", "slope_4", "slope_5", "slope_6",
+  "bush_1", "bush_2", "bush_3", "bush_4",
+}
+
 return factoring.from_atlas(
   "assets/sprites/atlases/solids.png", config.cell_size,
   {
@@ -12,13 +17,13 @@ return factoring.from_atlas(
     "wall_5",  "wall_6",  "wall_7",  "wall_8",  "hutwall_5",  "hutwall_6",  "hutwall_7",  "hutwall_8",
     "wall_9",  "wall_10", "wall_11", "wall_12", "hutwall_9",  "hutwall_10", "hutwall_11", "hutwall_12",
     "wall_13", "wall_14", "wall_15", "wall_16", "hutwall_13", "hutwall_14", "hutwall_15", "hutwall_16",
-    "slope_1", "slope_2", false,     "trunk_1", "trunk_2",    false,        false,        false,
-    "slope_3", "slope_4", "slope_5", "trunk_3", "trunk_4",    false,        false,        false,
+    "slope_1", "slope_2", false,     "trunk_1", "trunk_2",    "bush_1",     "bush_2",     "bush_5",
+    "slope_3", "slope_4", "slope_5", "trunk_3", "trunk_4",    "bush_3",     "bush_4",     "bush_6",
     false,     "slope_6", false,     "trunk_5", "trunk_6",    false,        false,        false,
   },
   function(codename)
     return {
-      transparent_flag = codename:starts_with("slope") or nil,
+      transparent_flag = transparents[codename],
       boring_flag = true,
       low_flag = lows[codename],
     }
