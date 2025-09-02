3fd3a4a83c924498ab0b28b36799ea53fb81430d
girvel <widauka@ya.ru>
2025-09-02 13:05:18 +0500

    Better leaves, add fern

--- levels/main/palette/tiles.lua
+++ levels/main/palette/tiles.lua
@@ -6,15 +6,13 @@ local walk_sounds = {
   dirt      = sound.multiple("assets/sounds/walk/walkway", .1),
   walkway_2 = sound.multiple("assets/sounds/walk/walkway", .1),
   planks    = sound.multiple("assets/sounds/walk/planks",  .1),
-  grass_1   = sound.multiple("assets/sounds/walk/grass",   .2),
-  grass_2   = sound.multiple("assets/sounds/walk/grass",   .2),
 }
 
 return factoring.from_atlas("assets/sprites/atlases/tiles.png", config.cell_size, {
-  "grass_1",  "grass_2",  "dirt",      "sand",      "leaves", "roots", "flowers_1", "planks",
-  false,      false,      "walkway_1", "walkway_2", "snow",   false,   "flowers_2", false,
-  "bricks_1", "bricks_2", false,       false,       false,    false,   "flowers_3", false,
-  "bricks_3", "bricks_4", false,       false,       false,    false,   "flowers_4", false,
+  "grass_1",  "grass_2",  "dirt",      "sand",      "roots",  "leaves_1", "flowers_1",  "planks",
+  false,      false,      "walkway_1", "walkway_2", "snow",   "leaves_2",  "flowers_2", false,
+  "bricks_1", "bricks_2", false,       false,       false,    false,       "flowers_3", false,
+  "bricks_3", "bricks_4", false,       false,       false,    false,       "flowers_4", false,
 }, function(codename)
   local s = walk_sounds[codename]
   return {
