f70ce4f6f4e550c40b0ddec9718b601b5e534c87
girvel <widauka@ya.ru>
2025-09-18 10:57:51 +0500

    snow sounds

--- levels/main/palette/on_tiles.lua
+++ levels/main/palette/on_tiles.lua
@@ -1,5 +1,12 @@
+local sound = require("engine.tech.sound")
 local factoring = require("engine.tech.factoring")
 local config = require("levels.main.config")
+
+local get_walk_sounds = function(codename)
+  if codename:starts_with("snow") then
+    return sound.multiple("assets/sounds/walk/snow", .4)
+  end
+end
 
 return factoring.from_atlas("assets/sprites/atlases/on_tiles.png", config.cell_size, {
   "snow_1",  "snow_2",  "snow_3",  "snow_4",  "fern",  false, false, false,
@@ -11,5 +18,11 @@ return factoring.from_atlas("assets/sprites/atlases/on_tiles.png", config.cell_s
   "carpet_9",  "carpet_10", "carpet_11", "carpet_12", false, false, false, false,
   "carpet_13", "carpet_14", "carpet_15", "carpet_16", false, false, false, false,
 }, function(codename)
-  return {boring_flag = true}
+  local walk_sounds = get_walk_sounds(codename)
+  return {
+    boring_flag = true,
+    sounds = walk_sounds and {
+      walk = walk_sounds,
+    },
+  }
 end)
