6e3b4b161041b585979cf528db5e74c5218124ec
girvel <widauka@ya.ru>
2025-09-20 15:44:46 +0500

    Finish the beach, sandcastles

--- levels/main/palette/on_tiles.lua
+++ levels/main/palette/on_tiles.lua
@@ -9,14 +9,14 @@ local get_walk_sounds = function(codename)
 end
 
 return factoring.from_atlas("assets/sprites/atlases/on_tiles.png", config.cell_size, {
-  "snow_1",  "snow_2",  "snow_3",  "snow_4",  "fern",  false, false, false,
-  "snow_5",  "snow_6",  "snow_7",  "snow_8",  "bones", false, false, false,
-  "snow_9",  "snow_10", "snow_11", "snow_12", "bonef", false, false, false,
-  "snow_13", "snow_14", "snow_15", "snow_16", false,   false, false, false,
-  "carpet_1",  "carpet_2",  "carpet_3",  "carpet_4",  false, false, false, false,
-  "carpet_5",  "carpet_6",  "carpet_7",  "carpet_8",  false, false, false, false,
-  "carpet_9",  "carpet_10", "carpet_11", "carpet_12", false, false, false, false,
-  "carpet_13", "carpet_14", "carpet_15", "carpet_16", false, false, false, false,
+  "snow",   "snow",   "snow",   "snow",   "fern",    false, false, false,
+  "snow",   "snow",   "snow",   "snow",   "bones",   false, false, false,
+  "snow",   "snow",   "snow",   "snow",   "bonef",   false, false, false,
+  "snow",   "snow",   "snow",   "snow",   "scastle", false, false, false,
+  "carpet", "carpet", "carpet", "carpet", "scastle", false, false, false,
+  "carpet", "carpet", "carpet", "carpet", false,     false, false, false,
+  "carpet", "carpet", "carpet", "carpet", false,     false, false, false,
+  "carpet", "carpet", "carpet", "carpet", false,     false, false, false,
 }, function(codename)
   local walk_sounds = get_walk_sounds(codename)
   return {
