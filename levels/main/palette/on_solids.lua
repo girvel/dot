bae32190c986030df00fa18a1ca6f5e6642c12e8
girvel <widauka@ya.ru>
2025-09-20 13:58:04 +0500

    Plates

--- levels/main/palette/on_solids.lua
+++ levels/main/palette/on_solids.lua
@@ -5,7 +5,7 @@ return factoring.from_atlas("assets/sprites/atlases/on_solids.png", config.cell_
   "vines", "grass_1",   "grass_2",   "cobweb_1", "cobweb_2", "statue_1", "statue_2", false,
   false,   "candles_1", "candles_2", "cobweb_3", "cobweb_4", "statue_3", "statue_4", false,
   "stage", "candles_3", "skull",     "dooro",    "statue_5", "statue_6", "statue_7", false,
-  "pot_1", "pot_2",     "pot_3",     false,      false,      false,      false,      false,
+  "pot_1", "pot_2",     "pot_3",     "plate",    "plate",    false,      false,      false,
   "pot_4", "pot_5",     "pot_6",     false,      false,      false,      false,      false,
 }, function(codename)
   return {boring_flag = true}
