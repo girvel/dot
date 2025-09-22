b56921f1cd024bbc0a6b3e20870b23d00dc9670d
girvel <widauka@ya.ru>
2025-09-22 14:32:51 +0500

    Eating food

--- levels/main/palette/on_solids.lua
+++ levels/main/palette/on_solids.lua
@@ -3,7 +3,18 @@ local config = require("levels.main.config")
 local interactive = require("engine.tech.interactive")
 local health      = require("engine.mech.health")
 
+
+local on_solids = {}
+
 local collect_berries = function(self, other)
+  State:remove(self)
+  health.heal(other, 1)
+end
+
+local collect_food = function(self, other)
+  self.on_remove = function()
+    State:add(on_solids.plate(), {position = self.position, grid_layer = "on_solids"})
+  end
   State:remove(self)
   health.heal(other, 1)
 end
@@ -13,11 +24,15 @@ local get_base = function(codename)
     local result = interactive.mixin(collect_berries)
     result.name = "ягоды"
     return result
+  elseif codename == "food" then
+    local result = interactive.mixin(collect_food)
+    result.name = "еда"
+    return result
   end
   return {}
 end
 
-return factoring.from_atlas("assets/sprites/atlases/on_solids.png", config.cell_size, {
+on_solids = factoring.from_atlas("assets/sprites/atlases/on_solids.png", config.cell_size, {
   "vines",    "vines",     "vines",     "cobweb_1", "cobweb_2", "statue_1", "statue_2", "window",
   false,      "candles_1", "candles_2", "cobweb_3", "cobweb_4", "statue_3", "statue_4", false,
   "stage",    "candles_3", "skull",     "dooro",    "statue_5", "statue_6", "statue_7", false,
@@ -30,3 +45,5 @@ return factoring.from_atlas("assets/sprites/atlases/on_solids.png", config.cell_
   result.boring_flag = true
   return result
 end)
+
+return on_solids
