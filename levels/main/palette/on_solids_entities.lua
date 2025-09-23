489bda71f31acf5a6043fbdfe9207716d6629281
girvel <widauka@ya.ru>
2025-09-23 13:16:30 +0500

    Pyre (first frame)

--- levels/main/palette/on_solids_entities.lua
+++ levels/main/palette/on_solids_entities.lua
@@ -1,4 +1,25 @@
-return Table.extend({},
+local animated = require "engine.tech.animated"
+
+
+local on_solids_entities = {}
+
+Table.extend(on_solids_entities,
   require("levels.main.palette.insects"),
   require("levels.main.palette.items_entities")
 )
+
+on_solids_entities.pyre_1 = function()
+  return Table.extend(animated.mixin("assets/sprites/animations/pyre", 1), {
+    codename = "pyre_1",
+    transparent_flag = true,
+  })
+end
+
+on_solids_entities.pyre_2 = function()
+  return Table.extend(animated.mixin("assets/sprites/animations/pyre", 2), {
+    codename = "pyre_2",
+    transparent_flag = true,
+  })
+end
+
+return on_solids_entities
--- levels/main/palette/solids_entities.lua
+++ levels/main/palette/solids_entities.lua
@@ -1,5 +1,6 @@
 local _common     = require("levels.main.palette._common")
 local player    = require("levels.main.palette.player")
+local animated  = require("engine.tech.animated")
 
 
 local solids_entities = {}
@@ -12,5 +13,19 @@ Table.extend(
   require("levels.main.palette.npcs"),
   require("levels.main.palette.wildlife")
 )
+
+solids_entities.pyre_3 = function()
+  return Table.extend(animated.mixin("assets/sprites/animations/pyre", 3), {
+    codename = "pyre_3",
+    transparent_flag = true,
+  })
+end
+
+solids_entities.pyre_4 = function()
+  return Table.extend(animated.mixin("assets/sprites/animations/pyre", 4), {
+    codename = "pyre_4",
+    transparent_flag = true,
+  })
+end
 
 return solids_entities
