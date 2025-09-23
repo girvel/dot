2f0d6e9481e5b3d7bf0ed13f2a94d4ce360426a7
girvel <widauka@ya.ru>
2025-09-23 14:45:11 +0500

    Getting a second knife from the chest

--- levels/main/scenes/init.lua
+++ levels/main/scenes/init.lua
@@ -1,4 +1,5 @@
-return Table.join(
+return Log.trace(Table.join(
   require("levels.main.scenes.000_base"),
-  require("levels.main.scenes.010_intro")
-)
+  require("levels.main.scenes.010_intro"),
+  require("levels.main.scenes.loc_1_upper_village")
+))
---
+++ levels/main/scenes/loc_1_upper_village.lua
@@ -0,0 +1,29 @@
+local iteration = require("engine.tech.iteration")
+local items_entities = require "levels.main.palette.items_entities"
+
+
+return {
+  loc_1_drop_knife = {
+    enabled = true,
+
+    --- @param self scene
+    --- @param dt number
+    start_predicate = function(self, dt)
+      return not State:exists(Runner.entities.knife_chest)
+    end,
+
+    --- @param self scene
+    run = function(self)
+      local placement
+      for d in iteration.rhombus(5) do
+        local p = d:add_mut(Runner.entities.knife_chest.position)
+        if not State.grids.solids:slow_get(p, true) and not State.grids.items:slow_get(p) then
+          placement = p
+          break
+        end
+      end
+
+      State:add(items_entities.knife(), {position = placement, grid_layer = "items"})
+    end,
+  },
+}
