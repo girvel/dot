c4d55f3a99cdd341e851c1c7112e07584c289755
girvel <widauka@ya.ru>
2025-09-30 14:58:56 +0500

    Implement 070_walking_under_aquule

---
+++ levels/main/scenes/2_forest/070_walking_under_aquule.lua
@@ -0,0 +1,54 @@
+local screenplay = require("engine.tech.screenplay")
+
+
+return {
+  --- @type scene|table
+  aquule_shader = {
+    enabled = true,
+    boring_flag = true,
+    mode = "sequential",
+
+    characters = {},
+
+    --- @param self scene|table
+    --- @param dt number
+    --- @param ch runner_characters
+    --- @param ps runner_positions
+    start_predicate = function(self, dt, ch, ps)
+      return true
+    end,
+
+    --- @param self scene|table
+    --- @param ch runner_characters
+    --- @param ps runner_positions
+    run = function(self, ch, ps)
+    end,
+  },
+
+  --- @type scene|table
+  _070_walking_under_aquule = {
+    enabled = true,
+
+    characters = {
+      player = {},
+    },
+
+    --- @param self scene|table
+    --- @param dt number
+    --- @param ch runner_characters
+    --- @param ps runner_positions
+    start_predicate = function(self, dt, ch, ps)
+      return (ch.player.position >= ps.aquule_start_1 and ch.player.position <= ps.aquule_end_1)
+          or (ch.player.position >= ps.aquule_start_2 and ch.player.position <= ps.aquule_end_2)
+    end,
+
+    --- @param self scene|table
+    --- @param ch runner_characters
+    --- @param ps runner_positions
+    run = function(self, ch, ps)
+      local sp = screenplay.new("assets/screenplay/070_walking_under_aquule.ms", ch)
+        sp:lines()
+      sp:finish()
+    end,
+  },
+}
