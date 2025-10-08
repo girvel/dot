2e5454d10b1804342dc5a3ef4c328633dc2a1a43
girvel <widauka@ya.ru>
2025-10-08 12:33:35 +0500

    Redo likka_following scene to not conflict with cutscenes

--- levels/main/scenes/2_forest/likka_following.lua
+++ levels/main/scenes/2_forest/likka_following.lua
@@ -5,6 +5,12 @@ local async = require("engine.tech.async")
 local NEUTRAL_DISTANCE = 4
 local ADHD_PERIOD = 15
 
+local needs_travel = function(likka)
+  if State.combat then return false end
+  local distance = (likka.position - State.player.position):abs2()
+  return distance <= 1 or distance > NEUTRAL_DISTANCE
+end
+
 return {
   --- @type scene|table
   likka_following = {
@@ -16,45 +22,38 @@ return {
     },
 
     start_predicate = function(self, dt, ch, ps)
-      return true
+      -- not every tick to not potentially block cutscenes
+      return State.period:absolute(.1, self, "start")
     end,
 
+    _last_action_t = nil,
+
     run = function(self, ch, ps)
-      local needs_travel = function()
-        if State.combat then return false end
-        local distance = (ch.likka.position - State.player.position):abs2()
-        return distance <= 1 or distance > NEUTRAL_DISTANCE
-      end
+      -- one iteration at a time, because needs to be disabled in cutscenes
+      if needs_travel(ch.likka) then
+        local target = State.player.position
+        local norm = (target - ch.likka.position):normalized2()
+        local shift = norm:rotate()
 
-      local last_action_t = love.timer.getTime()
-      while self.enabled do
-        if needs_travel() then
-          local target = State.player.position
-          local norm = (target - ch.likka.position):normalized2()
-          local shift = norm:rotate()
-
-          for _, offset in ipairs {Vector.zero, shift, -shift} do
-            local path = api.build_path(ch.likka.position, target - norm * 2 + offset)
-            if path then
-              api.follow_path(ch.likka, path, false, 7.5)
-              api.rotate(ch.likka, State.player)
-              last_action_t = love.timer.getTime()
-              break
-            end
+        for _, offset in ipairs {Vector.zero, shift, -shift} do
+          local path = api.build_path(ch.likka.position, target - norm * 2 + offset)
+          if path then
+            api.follow_path(ch.likka, path, false, 7.5)
+            api.rotate(ch.likka, State.player)
+            self._last_action_t = love.timer.getTime()
+            break
           end
         end
+      end
 
-        if love.timer.getTime() - last_action_t >= ADHD_PERIOD
-          and State.period:absolute(1, self, "ADHD")
-          and Random.chance(.3)
-        then
-          last_action_t = love.timer.getTime()
-          ch.likka:rotate(Random.choice(Vector.directions))
-          async.sleep(Random.float(.5, 3))
-          ch.likka:rotate(Random.choice(Vector.directions))
-        end
-
-        coroutine.yield()
+      if love.timer.getTime() - self._last_action_t >= ADHD_PERIOD
+        and State.period:absolute(1, self, "ADHD")
+        and Random.chance(.3)
+      then
+        self._last_action_t = love.timer.getTime()
+        ch.likka:rotate(Random.choice(Vector.directions))
+        async.sleep(Random.float(.5, 3))
+        ch.likka:rotate(Random.choice(Vector.directions))
       end
     end,
   },
