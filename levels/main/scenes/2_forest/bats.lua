922093aa68a7fc5cab3fe2a1dc104706f409ae03
girvel <widauka@ya.ru>
2025-10-09 14:38:03 +0500

    More bats

--- levels/main/scenes/2_forest/bats.lua
+++ levels/main/scenes/2_forest/bats.lua
@@ -2,22 +2,45 @@ local health = require("engine.mech.health")
 local wildlife = require "levels.main.palette.wildlife"
 
 
+local BATS_NS = {7, 5, 6, 11}
+
 return {
   --- @type scene|table
-  bats_1 = {
+  bats = {
     enabled = true,
+    mode = "sequential",
+
     characters = {
       player = {},
     },
 
-    start_predicate = function(self, dt, ch, ps)
-      return (ch.player.position - ps.bats_1):abs2() <= 3
+    _ps = {},
+
+    on_add = function(self, ch, ps)
+      for name, p in pairs(ps) do
+        if name:starts_with("bats_") then
+          self._ps[tonumber(name:sub(6))] = p
+        end
+      end
     end,
 
-    run = function(self, ch, ps)
+    start_predicate = function(self, dt, ch, ps)
+      for i, p in pairs(self._ps) do
+        if (ch.player.position - p):abs2() <= 3 then
+          return i, p
+        end
+      end
+    end,
+
+    run = function(self, ch, ps, i, activated_p)
+      self._ps[i] = nil
+      if Table.count(self._ps) == 0 then
+        self.enabled = false
+      end
+
       local counter = 0
       local to_combat = {ch.player}
-      local bfs = State.grids.solids:bfs(ch.player.position)
+      local bfs = State.grids.solids:bfs(activated_p)
       bfs()
 
       for p, e in bfs do
@@ -32,7 +55,7 @@ return {
           table.insert(to_combat, e)
           counter = counter + 1
 
-          if counter == 7 then break end
+          if counter == BATS_NS[i] then break end
         end
 
         ::continue::
