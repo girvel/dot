8179dd5a6cc561272afd4e73ba6fb17e80ec2538
girvel <widauka@ya.ru>
2025-10-13 15:16:45 +0500

    Add bird sounds (shitty for now)

--- levels/main/palette/sounds_invisible.lua
+++ levels/main/palette/sounds_invisible.lua
@@ -3,8 +3,11 @@ local sound = require("engine.tech.sound")
 
 local sounds_invisible = {}
 
-sounds_invisible.birds_1 = function()
-  return sound.source("assets/sounds/birds_1", .2, "medium")
+for i = 1, 3 do
+  local name = "birds_" .. i
+  sounds_invisible[name] = function()
+    return sound.source("assets/sounds/" .. name, .2, "medium")
+  end
 end
 
 Ldump.mark(sounds_invisible, "const", ...)
