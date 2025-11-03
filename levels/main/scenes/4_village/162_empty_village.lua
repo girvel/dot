439216fc7ac13587230bd0434c56d7eebe159c15
girvel <widauka@ya.ru>
2025-11-03 12:57:45 +0500

    Implement 162_empty_village

---
+++ levels/main/scenes/4_village/162_empty_village.lua
@@ -0,0 +1,22 @@
+local screenplay = require("engine.tech.screenplay")
+
+
+return {
+  --- @type scene
+  _162_empty_vilage = {
+    enabled = true,
+    characters = {
+      player = {},
+    },
+
+    start_predicate = function(self, dt, ch, ps)
+      return ch.player.position.y <= ps.empty_village_y.y
+    end,
+
+    run = function(self, ch, ps)
+      local sp = screenplay.new("assets/screenplay/162_empty_village.ms", ch)
+        sp:lines()
+      sp:finish()
+    end,
+  },
+}
